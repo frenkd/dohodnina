2f71df99b65484579549860ae82df7456c226baa
Frenk Dragar <dragarfrenk@gmail.com>
2022-01-23 14:40:09 +0100

    ADD Student and Employed Calculations

---
+++ student.py
@@ -0,0 +1,95 @@
+from dohodnina import splosna_olajsava, letna_dohodnina_lestvica
+
+
+def izracunaj_dohodnino(bruto_zasluzek,
+                        invalid=False,
+                        prijavljen_kot_vzdrzevan=True,
+                        stroski_prevoza_in_nocitve=0,
+                        izpisi=False):
+
+    # =======================
+    # Prispevki (za pokojninsko in invalidsko zavarovanje)
+    # =======================
+    # Študenti:  15.5%
+    prispevki = bruto_zasluzek * 0.155
+
+    # =======================
+    # Davčne olajšave (letno)
+    # =======================
+    # 1. Splošna olajšava
+    # Upošteva se, če te starši ne prijavijo kot vzdrževanega otroka oz. druzinskega člana
+    znesek_splosna_olajsava = splosna_olajsava(
+        bruto_zasluzek) if not prijavljen_kot_vzdrzevan else 0
+
+    # 2. Osebne olajsave za 100% invalide
+    znesek_olajsava_za_invalide = 17658.84 if invalid else 0
+
+    # 3. Posebna osebna olajšava
+    # Za rezidenta, ki se izobražuje in ima status dijaka ali študenta, znaša 3.500,00 eura.
+    znesek_posebna_osebna_olajsava = 3500
+    # Davčna osnova je zmanjšana za normirane stroške v višini 10 % od doseženega dohodka
+    znesek_normirani_stroski = bruto_zasluzek * 0.10
+    # Poleg normiranih stroškov lahko dijak ali študent uveljavlja tudi dejanske stroške prevoza in nočitve
+    znesek_stroski_prevoza_in_nocitve = stroski_prevoza_in_nocitve
+    znesek_dodatna_studentska_olajsava = znesek_normirani_stroski + \
+        znesek_stroski_prevoza_in_nocitve
+
+    # 4. Posebna olajšava
+    # Predpostavka (v robnem primeru verjetno zmotna),
+    # da večina študentov nima vzdrževanih otrok/družinskih članov
+
+    # 4. Olajšava za prostovoljno dodatno pokojninsko zavarovanje
+    # Predpostavka (v robnem primeru verjetno zmotna),
+    # da večina študentov ne razmišlja o dodatnem pokojninskem zavarovanju
+    # (ali pa da bomo dočakali penzijo lol)
+
+    # =======================
+    # Poračun
+    # =======================
+    olajsave = znesek_splosna_olajsava \
+        + znesek_olajsava_za_invalide \
+        + znesek_posebna_osebna_olajsava \
+        + znesek_dodatna_studentska_olajsava \
+
+    # Neto letna davcna osnova
+    neto_davcna_osnova = bruto_zasluzek - prispevki - olajsave
+    # Omejimo se na pozitivne vsote
+    neto_davcna_osnova = max(neto_davcna_osnova, 0)
+
+    # Stopnja dohodnine (letno)
+    letna_dohodnina = letna_dohodnina_lestvica(neto_davcna_osnova)
+
+    if izpisi:
+        print(f"Bruto letni zaslužek: {bruto_zasluzek:.2f}€")
+        print(f"Prispevki: {prispevki:.2f}€")
+        print(f"'Neto' letni zaslužek: {(bruto_zasluzek - prispevki):.2f}€")
+        print(f"Olajšave skupaj: {olajsave:.2f}€")
+        print(f"Neto letna davčna osnova: {neto_davcna_osnova:.2f}€")
+        print(f"Letna dohodnina: {letna_dohodnina:.2f}€")
+        print(f"Dejanski neto letni zaslužek: {(bruto_zasluzek - prispevki - letna_dohodnina):.2f}€")
+
+    return letna_dohodnina
+
+
+if __name__ == "__main__":
+    print("***| Informativni izracun dohodnine za študenta |***\n")
+    bruto_letni_zasluzek = float(input("Vnesite svoj bruto letni zasluzek: "))
+
+    prijavljen_kot_vzdrzevan = bool(
+        input("Vas bodo starši prijavili kot vzdrževanega otroka/družinskega člana? (y/N): ").lower() == 'y')
+
+    invalid = input(
+        "Ste invalid s 100% telesno okvaro? (y/N): ").lower() == 'y'
+
+    stroski_prevoza_in_nocitve = float(
+        input("Uveljavljeni dejanski stroški prevoza in nočitve (eur): "))
+
+    print("\nIzračun:\n")
+
+    izracunaj_dohodnino(
+        bruto_zasluzek=bruto_letni_zasluzek,
+        prijavljen_kot_vzdrzevan=prijavljen_kot_vzdrzevan,
+        invalid=invalid,
+        stroski_prevoza_in_nocitve=stroski_prevoza_in_nocitve,
+        izpisi=True
+    )
---
+++ zaposlen.py
@@ -0,0 +1,104 @@
+from dohodnina import splosna_olajsava, letna_dohodnina_lestvica, olajsava_vzdrzevani_otroci, olajsava_vzdrzevani_druzinski_clani
+
+
+def izracunaj_dohodnino(bruto_zasluzek,
+                        invalid=False,
+                        stevilo_vzdrzevanih_otrok=0,
+                        stevilo_vzdrzevanih_druzinskih_clanov=0,
+                        stevilo_vzdrzevanih_mesecev=12,
+                        premija_dod_pok_zav=0,
+                        izpisi=False):
+
+    # =======================
+    # Prispevki (za pokojninsko in invalidsko zavarovanje)
+    # =======================
+    # Zaposleni:  21.1%
+    prispevki = bruto_zasluzek * 0.221
+
+    # =======================
+    # Davčne olajšave (letno)
+    # =======================
+    # 1. Splošna olajšava
+    znesek_splosna_olajsava = splosna_olajsava(bruto_zasluzek)
+
+    # 2. Osebne olajsave za 100% invalide
+    znesek_olajsava_za_invalide = 17658.84 if invalid else 0
+
+    # 3. Posebna osebna olajšava
+    # Ne pride v poštev za zaposlene (samo za študente in dijake)
+
+    # 4. Posebna olajšava
+    # 4.1 za vzdrževane otroke
+    olajsava_za_vzdrzevane_otroke = olajsava_vzdrzevani_otroci(
+        stevilo_vzdrzevanih_otrok) * (stevilo_vzdrzevanih_mesecev/12)
+    # 4.2za vsakega drugega vzdrževanega družinskega člana
+    olajsava_za_vzdrzevane_druzinske_clane = olajsava_vzdrzevani_druzinski_clani(
+        stevilo_vzdrzevanih_druzinskih_clanov) * (stevilo_vzdrzevanih_mesecev/12)
+
+    # 5. Olajšava za prostovoljno dodatno pokojninsko zavarovanje
+    # Največ do zneska premije, ki je enak 24 % obveznih prispevkov za
+    # pokojninsko in invalidsko zavarovanje za zavarovanca
+    # Oziroma 5,844% pokojnine zavarovanca
+    znesek_olajsava_za_dodatno_pokojnino = min(premija_dod_pok_zav, 0.24 * prispevki)
+    # Ne več kot 2.819,09 eurov letno.
+    znesek_olajsava_za_dodatno_pokojnino = min(znesek_olajsava_za_dodatno_pokojnino, 2819.09)
+
+    # =======================
+    # Poračun
+    # =======================
+    olajsave = znesek_splosna_olajsava \
+        + znesek_olajsava_za_invalide \
+        + olajsava_za_vzdrzevane_otroke \
+        + olajsava_za_vzdrzevane_druzinske_clane \
+        + znesek_olajsava_za_dodatno_pokojnino
+
+    # Neto letna davčna osnova
+    neto_davcna_osnova = bruto_zasluzek - prispevki - olajsave
+    # Omejimo se na pozitivne vsote
+    neto_davcna_osnova = max(neto_davcna_osnova, 0)
+
+    # Stopnja dohodnine (letno)
+    letna_dohodnina = letna_dohodnina_lestvica(neto_davcna_osnova)
+
+    if izpisi:
+        print(f"Bruto letni zaslužek: {bruto_zasluzek:.2f}€")
+        print(f"Prispevki: {prispevki:.2f}€")
+        print(f"Olajšave skupaj: {olajsave:.2f}€")
+        print(f"Neto letna davčna osnova: {neto_davcna_osnova:.2f}€")
+        print(f"Letna dohodnina: {letna_dohodnina:.2f}€")
+        print(
+            f"Neto letni zaslužek: {(bruto_zasluzek - prispevki -letna_dohodnina):.2f}€")
+
+    return letna_dohodnina
+
+
+if __name__ == "__main__":
+    print("***| Informativni izracun dohodnine za zaposlenega |***")
+    bruto_letni_zasluzek = float(input("Vnesite svoj bruto letni zasluzek: "))
+
+    invalid = input(
+        "Ste invalid s 100% telesno okvaro? (y/N): ").lower() == 'y'
+
+    stevilo_vzdrzevanih_otrok = int(input(
+        "Število vzdrževanih otrok (starost <18) (0-?): "))
+    stevilo_vzdrzevanih_druzinskih_clanov = int(input(
+        "Število vzdrževanih družinskih članov (starost 18-26) (0-?): "))
+    if stevilo_vzdrzevanih_otrok > 0 or stevilo_vzdrzevanih_druzinskih_clanov > 0:
+        stevilo_vzdrzevanih_mesecev = int(
+            input("Število mesecev (starost 18-26) (0-12): "))
+    else:
+        stevilo_vzdrzevanih_mesecev = 12
+
+    premija_dod_pok_zav = float(input("Letni znesek premije za dodatno pokojninsko zavarovanje (v eur): "))
+
+    print("\nInformativni izračun: \n")
+
+    izracunaj_dohodnino(
+        bruto_zasluzek=bruto_letni_zasluzek,
+        invalid=invalid,
+        stevilo_vzdrzevanih_otrok=stevilo_vzdrzevanih_otrok,
+        stevilo_vzdrzevanih_druzinskih_clanov=stevilo_vzdrzevanih_druzinskih_clanov,
+        stevilo_vzdrzevanih_mesecev=stevilo_vzdrzevanih_mesecev,
+        premija_dod_pok_zav=premija_dod_pok_zav,
+        izpisi=True
+    )
